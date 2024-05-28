21b78ecd7a212626b6634e0523a3d1dd61d3f77c
Anton Masharov <aommminc@gmail.com>
2024-05-28 13:33:02 +0500

    feat: opportunity to make similarity coefficient slider non-interactive

--- POI-DNA-Analyzer/obj/Debug/net8.0-windows/POI-DNA-Analyzer.AssemblyInfo.cs
+++ POI-DNA-Analyzer/obj/Debug/net8.0-windows/POI-DNA-Analyzer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POI-DNA-Analyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8959f68b9a394bfe7a176fec3286a85e3225d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a6c2a7a6cbe70c74fcbe57bc1a0bffb3ebc39a")]
 [assembly: System.Reflection.AssemblyProductAttribute("POI-DNA-Analyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POI-DNA-Analyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
