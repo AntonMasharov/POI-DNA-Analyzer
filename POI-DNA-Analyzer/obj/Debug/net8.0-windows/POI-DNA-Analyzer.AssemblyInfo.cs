255a9aa7af0c81278a844cd809eec6c91c434278
Anton Masharov <aommminc@gmail.com>
2024-05-31 09:30:08 +0500

    refactor: deleted unnecessary dependencies

--- POI-DNA-Analyzer/obj/Debug/net8.0-windows/POI-DNA-Analyzer.AssemblyInfo.cs
+++ POI-DNA-Analyzer/obj/Debug/net8.0-windows/POI-DNA-Analyzer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POI-DNA-Analyzer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be4d37cbb7932eb18c87dd2a0292860f467c9786")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8844a81703c519b089420e817d54ce32bef978e")]
 [assembly: System.Reflection.AssemblyProductAttribute("POI-DNA-Analyzer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POI-DNA-Analyzer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
